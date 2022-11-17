de5f2d06069731541a48ce97c760e33b4d883aa0
Josh <jrlewis312gmail.com>
2022-11-17 15:24:34 -0800

    Phase 2 Done

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 
 const { environment } = require('./config');
+const { ValidationError } = require('sequelize');
 const isProduction = environment === 'production';
 
 const routes = require('./routes');
@@ -40,5 +41,38 @@ app.use(
   })
 );
 app.use(routes);
+
+// ERROR HANDLING MIDDLEWARE
+// "Resource not found" Error-Handler
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
+
+// Sequelize Error-Handler
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
+
+// Error Formatting Error-Hanlder
+// SHOULD BE FINAL ERROR HANDLER IN app.js
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
