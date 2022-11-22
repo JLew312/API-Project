f0333289a0bb4dfdfe4509f9b99ced92a8843c71
Josh <jrlewis312gmail.com>
2022-11-22 14:05:23 -0800

    association built between host and spot models

--- backend/db/models/host.js
+++ backend/db/models/host.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Host.hasMany(
+        models.Spot,
+        {foreignKey: 'ownerId', onDelete: 'CASCADE'}
+      )
     }
   }
   Host.init({
