de487f4c8aade20605d9628fbea7d7e6758d41dd
Josh <jrlewis312gmail.com>
2022-11-26 15:32:15 -0800

    double quoted table names

--- backend/db/migrations/20221122221416-create-spot-image.js
+++ backend/db/migrations/20221122221416-create-spot-image.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('spotImages', {
+    await queryInterface.createTable("spotImages", {
       id: {
         allowNull: false,
         autoIncrement: true,
--- backend/db/migrations/20221122233716-create-review.js
+++ backend/db/migrations/20221122233716-create-review.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('reviews', {
+    await queryInterface.createTable("reviews", {
       id: {
         allowNull: false,
         autoIncrement: true,
